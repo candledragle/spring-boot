7532d356ecca660d603532ba75eca27970a87e71
shengyanming <shengyanming@altamob.com>
2019-04-01 12:13:07 +0800

    添加参考链接

--- spring-boot-samples/spring-boot-sample-async/src/main/java/sample/configure/TestConfigurationProperties.java
+++ spring-boot-samples/spring-boot-sample-async/src/main/java/sample/configure/TestConfigurationProperties.java
@@ -3,6 +3,10 @@ package sample.configure;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/*
+* https://www.cnblogs.com/liaojie970/p/8043150.html
+* */
+
 @Component
 @ConfigurationProperties(prefix = "spring.adapter.redisson")
 public class TestConfigurationProperties {
