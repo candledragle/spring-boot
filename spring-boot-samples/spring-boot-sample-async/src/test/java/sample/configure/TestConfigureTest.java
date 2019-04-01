340e19c01d7dec7ab0781f8b1b2123698ab9161e
shengyanming <shengyanming@altamob.com>
2019-04-01 11:28:16 +0800

    junit5

---
+++ spring-boot-samples/spring-boot-sample-async/src/test/java/sample/configure/TestConfigureTest.java
@@ -0,0 +1,25 @@
+package sample.configure;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import sample.bean.TestBean;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+class TestConfigureTest {
+
+	private Log loger = LogFactory.getLog(TestConfigureTest.class);
+
+	@Resource
+	TestBean testBean;
+
+	@Test
+	void testBean() {
+
+		String s = testBean.sayHello();
+		loger.info(s);
+	}
+}
